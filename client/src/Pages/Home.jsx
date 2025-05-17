3a2625774feab2c1ca12ad82b5c98825dfcdb760
iamashish17 <acharya2074@gmail.com>
2025-05-17 15:23:19 +0545

    HotelBooking

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import FeatureDestination from '../Components/FeatureDestination'
+import ExclusiveOffer from '../Components/ExclusiveOffer'
+import Testimonial from '../Components/Testimonial'
+import NewsLetter from '../Components/NewsLetter'
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <FeatureDestination /> 
+      <ExclusiveOffer />
+      <Testimonial />
+      <NewsLetter />
+    </>
+  )
+}
+
+export default Home
