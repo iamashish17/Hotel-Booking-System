6bd61245bae9c9486d74f394cca6a9f32c090b98
iamashish17 <acharya2074@gmail.com>
2025-05-17 16:12:51 +0545

    added listing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './Pages/Home';
+import AllRooms from './Pages/AllRooms';
 import Footer from './Components/Footer';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/rooms' element={<AllRooms/>}/>
         </Routes>
       </div>
       <Footer />
---
+++ client/src/Pages/AllRooms.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AllRooms = () => {
+  return (
+    <div className="flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24">
+     <div>
+      <div>
+        <h1 className='font-playfair text-4xl mf:text-[40px]'>Hotel Rooms</h1>
+        <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories.</p>
+      </div>
+     </div>
+     {/*Filters */}
+     <div>
+
+     </div>
+    </div>
+
+  );
+};
+
+export default AllRooms;
