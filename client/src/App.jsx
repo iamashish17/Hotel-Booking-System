1388ceed7544e5a3882bea68c99e5481a3b9cd1d
iamashish17 <acharya2074@gmail.com>
2025-06-03 15:37:36 +0545

    addroom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,39 @@
-import Navbar from './Components/Navbar'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './Pages/Home';
-import AllRooms from './Pages/AllRooms';
-import Footer from './Components/Footer';
-import RoomDetail from './Pages/RoomDetail';
-import MyBooking from './Pages/MyBooking';
-import HotelReg from './Components/HotelReg';
-import Layout from './Pages/hotelOwner/Layout';
-import DashBoard from './Pages/hotelOwner/DashBoard';
-import AddRoom from './Pages/hotelOwner/AddRoom';
-import ListRoom from './Pages/hotelOwner/ListRoom';
+import Navbar from "./Components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./Pages/Home";
+import AllRooms from "./Pages/AllRooms";
+import Footer from "./Components/Footer";
+import RoomDetail from "./Pages/RoomDetail";
+import MyBooking from "./Pages/MyBooking";
+import HotelReg from "./Components/HotelReg";
+import Layout from "./Pages/hotelOwner/Layout";
+import DashBoard from "./Pages/hotelOwner/DashBoard";
+import AddRoom from "./Pages/hotelOwner/AddRoom";
+import ListRoom from "./Pages/hotelOwner/ListRoom";
 
 const App = () => {
-
   const isOwnerPath = useLocation().pathname.includes("owner");
 
   return (
     <div>
-      {!isOwnerPath && <Navbar /> }
-      { false && <HotelReg />}
-      <div className='min-h-[70vh]'>
+      {!isOwnerPath && <Navbar />}
+      {false && <HotelReg />}
+      <div className="min-h-[70vh]">
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/rooms' element={<AllRooms/>}/>
-          <Route path='/rooms/:id' element={<RoomDetail/>}/>
-          <Route path='/my-bookings' element={<MyBooking/>}/>
-          <Route path='/owner' element={<Layout/>}>
-              <Route index element ={<DashBoard />} />
-              <Route index element ={<AddRoom />} />
-              <Route index element ={<ListRoom />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
+          <Route path="/rooms/:id" element={<RoomDetail />} />
+          <Route path="/my-bookings" element={<MyBooking />} />
+          <Route path="/owner" element={<Layout />}>
+            <Route index element={<DashBoard />} />
+            <Route path="add-room" element={<AddRoom />} />
+            <Route path="list-room" element={<ListRoom />} />
           </Route>
         </Routes>
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/Pages/hotelOwner/AddRoom.jsx
+++ client/src/Pages/hotelOwner/AddRoom.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
+import Title from '../../Components/Title'
 
 const AddRoom = () => {
 
@@ -24,7 +25,7 @@ const [inputs, setInputs] = useState({
   return (
     <form>
       <Title align='left' font='outfit' title='Add Room' subTitle='fill in the details carefully and accurate room details, pricing, and amenitites, to enhance the user booking experience.'/>
-      {/* UPLOAD area for Images */}
+      {/* Images */}
       <p className='text-gray-800-mt-10'>Images</p>
       <div className='grid grid-cols-2 sm:flex gap-4 my-2 flex-wrap'>
         {Object.keys(images).map((key) => (
@@ -35,6 +36,41 @@ const [inputs, setInputs] = useState({
           </label>
         ))}
       </div>
+
+        <div className='w-full flex max-sm:flex-col sm:gap-4 mt-4'>
+          <div className='flex-1 max-w-48'>
+            <p className='text-grahy-800 mt-4'>Room Type</p>
+            <select value={inputs.roomType} onChange={e=> setInputs({...inputs, roomType:e.target.value})}
+            className='border opacity-70 border-gray-300 mt-1 rounded p-2 w-full'>
+              <option value="">Select Room Type</option>
+              <option value="Single Room">Single Bed</option>
+              <option value="Double Room">Double Bed</option>
+              <option value="Luxury Room">Luxury Bed</option>
+              <option value="Family Suite">Family Suite</option>
+            </select>
+          </div>
+          <div>
+            <p className='mt-4 text-gray-800'>
+              Price <span className='text-xs'>/nights</span>
+            </p>
+            <input type="number" placeholder='0' className='border border-gray-300 mt-1 rounded p-2 w-24' value={inputs.pricePerNight} onChange={e=>setInputs({...inputs, pricePerNight: e.target.value})}/>
+          </div>
+        </div>
+        <p className='text-gray-800 mt-4'>Amenities</p>
+        <div className='flex flex-col flex-wrap mt-1 text-gray-400 max-w-sm'>
+          {Object.keys(inputs.amenities).map((amenity, index) => (
+            <div key={index}>
+              <input type="checkbox" id={`amenities${index+1}`} checked={inputs.amenities[amenity]} onChange={() => setInputs({...inputs,
+                amenities: {...inputs.amenities, [amenity]: !inputs.amenities
+                  [amenity]}
+              })}/>
+              <label htmlFor={`amenities${index+1}`}> {amenity}</label>
+            </div>
+          ))}
+        </div>
+        <button className='bg-primary text-white px-8 py-2 rounded mt-8 cursor-pointer'>
+          Add Room
+        </button>
     </form>
   )
 }
